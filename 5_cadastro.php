ba7b58357d69e8d64a737e5103f14ee53e070ace
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-18 15:03:18 -0300

    Resolução exercício 5

--- 5_cadastro.php
+++ 5_cadastro.php
@@ -18,8 +18,28 @@
         <button type="submit">Cadastrar</button>
     </form>
     <?php
+ 
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // receba os valores enviados pelo formulario 
+        $nome = $_POST['nome'];
+        $senha = $_POST['senha'];
 
-    // Digitar PHP (1º Aqui)
+    // abre o arquivo usuario.txt para escrita
+    // adiciona dados no final do arquivo
+        $arquivo = fopen('usuarios.txt', 'a');
+
+    // cria linha com nome e senha separados por ";"
+        $linha = $nome . ';' . $senha . '\n';
+
+    // escreve a linha no arquivo
+        fwrite($arquivo, $linha);
+
+    // fecha o arquivo 
+        fclose($arquivo);
+
+    echo "<p> Usuário cadastro com sucesso!</p>";
+
+    }
     
     ?>
 </body>
