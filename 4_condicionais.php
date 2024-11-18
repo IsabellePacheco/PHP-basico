44884c619d91ca204d139b22a3a849e50b4ba994
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-18 14:23:46 -0300

    Resolução exercício 4

--- 4_condicionais.php
+++ 4_condicionais.php
@@ -1,6 +1,21 @@
 <?php
 
-// Digitar PHP (1º Aqui)
+// Verificar os dados do formulário
+// Utilizando o método POST
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $senha = $_POST['senha'];
+
+    if($senha === '1234567'){
+        // redireciona para a página de "boas vindas"
+        header("Location: 4b_bem_vindo.php");
+        exit();
+    }
+}else{
+    // mensagem de erro
+    $erro = "Senha incorreta. Tente novamente!";
+}
 
 ?>
 <!DOCTYPE html>
@@ -18,11 +33,10 @@
     </form>
 
     <?php
-
-    // Digitar PHP (2º Aqui)
+    if(isset($erro)){
+        echo "<p style='color: red;'> $erro </p>";
+    }
 
     ?>
 </body>
-</html>
-
-
+</html?
