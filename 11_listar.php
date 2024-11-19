055595a5f7a874ece81d52649ff471cc09d63f9d
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-19 16:01:13 -0300

    Resolução exercício 11

--- 11_listar.php
+++ 11_listar.php
@@ -13,7 +13,7 @@ if ($conn->connect_error) {
 }
 
 // consulta os dados da tabela clientes
-$sql ="SELECT id, nome, email FROM clientes";
+$sql ="SELECT id, nome, email, telefone FROM clientes";
 $result = $conn->query ($sql);
 
 // verifica se existe registros e os exibe em formato de tabela
@@ -24,14 +24,16 @@ if ($result->num_rows > 0){
     echo "<table border='2'>";
 
         // define cabeçalho da tabela
-        echo "<tr><th>Nome</th><th>Email</th><th>Telefone</th></tr>";
+        echo "<tr><th>id</th><th>Nome</th><th>Email</th><th>Telefone</th></tr>";
 
         // enquanto tiver dados preenchidos no BD
         // Listar e exibir em formato de tabela
         while ($row = $result->fetch_assoc()){
             echo "<tr>";
+            echo "<td>" . $row['id'] . "</td>";
             echo "<td>" . $row['nome'] . "</td>";
             echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['telefone'] . "</td>";
             echo "</tr>";
         }
     echo "</table>";
