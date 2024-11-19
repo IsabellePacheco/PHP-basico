505206d3b0e9401ea683a647d085eb79fce7fa36
Isabelle Pacheco <isapachecosenai@gmail.com>
2024-11-19 11:58:30 -0300

    Resolução exercício 13

--- 13_exclusao.php
+++ 13_exclusao.php
@@ -1,9 +1,9 @@
 <!-- Passar id via URL -->
-<!-- http://localhost/php-basico-Alunos/13_exclusao.php?id=2-->
+<!-- http://localhost/php-basico-IsabellePacheco/13_exclusao.php?id=2-->
 
 <?php
 // Conecta ao banco de dados
-$servername = "localhost";
+$servername = "localhost:3309";
 $username = "root";
 $password = "";
 $dbname = "exercicio";
@@ -16,8 +16,20 @@ if ($conn->connect_error) {
 }
 
 
-// Digitar PHP + SQL (1º Aqui)
+// Verifica se um ID foi passado via URL para exclusão
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    
 
+    // deleta o registro do cliente com ID especifico
+    $sql = "DELETE FROM clientes WHERE id='$id' ";
+
+    if ($conn->query($sql) === TRUE) { 
+        echo "<p> Cliente excluido com sucesso!</p>";
+    }else{
+        echo "<p>Erro ao excluir clientes:". $conn->error . "</p>";
+    }
+}
 
 // Fecha a conexão
 $conn->close();
